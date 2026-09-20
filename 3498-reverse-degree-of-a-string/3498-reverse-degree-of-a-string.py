13886fcb33423b6e7e229c839347898c3f06206c
Yohannes Alemu Geremew <122824502+johannesalemu01@users.noreply.github.com>
2026-09-20 13:07:23 +0300

    Time: 23 ms (5.57%), Space: 19.3 MB (19.13%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,11 +1,14 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         
-        alpha=[]
-        # product=1
+        # alpha=[]
         sum=0
-        for i in range(ord('z'),ord('a')-1,-1):
-            alpha.append(chr(i))
+
+        alpha=[chr(i) for i in range(ord('z'),ord('a')-1,-1)]
+        print(alpha)
+
+        # for i in range(ord('z'),ord('a')-1,-1):
+        #     alpha.append(chr(i))
 
         for i,char in enumerate(s):
             i+=1
